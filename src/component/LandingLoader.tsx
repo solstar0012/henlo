6456b10db7e92ffa39ab58a022b04627bd3e8345
fulldev0023 <arakiyamahashi@gmail.com>
2024-09-27 23:46:15 +0200

    fix one

--- src/component/LandingLoader.tsx
+++ src/component/LandingLoader.tsx
@@ -4,7 +4,7 @@ const LandingLoader = () => {
   return (
     <div className='flex flex-col relative pt-32 justify-center items-center gap-20 h-screen w-full'>
       <img
-        src='/coat.png'
+        src='/hello.png'
         className='absolute w-32 top-1/4'
         alt='A cute dog avatar'
       />
